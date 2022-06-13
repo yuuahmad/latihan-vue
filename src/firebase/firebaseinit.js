7fe43572c1c78dbfedb35f985979281ea2852a11
yuuahmad <ahmadyusufmaulana0@gmail.com>
2022-06-13 16:11:39 +0700

    setup pertama

---
+++ src/firebase/firebaseinit.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAfe4l1PMhmjchHSX8m2-KKhBRuPO8pWp0",
+    authDomain: "latihan-vue-11.firebaseapp.com",
+    projectId: "latihan-vue-11",
+    storageBucket: "latihan-vue-11.appspot.com",
+    messagingSenderId: "67654669236",
+    appId: "1:67654669236:web:24d8a1e79084099fce98e1"
+};
